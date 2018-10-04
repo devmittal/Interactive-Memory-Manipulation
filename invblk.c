fba29ecd74a9535d5b5770a7e9d528e477e7f341
Devansh Mittal <demi1671@colorado.edu>
2018-10-03 21:47:43 -0600

    Added comments and file details

--- invblk.c
+++ invblk.c
@@ -1,3 +1,14 @@
+/*****************************************************************************
+​ ​* ​ ​ @file​ ​  invblk.c
+​ * ​ ​ @brief​ ​ This file inverts the data bits present at the addresses
+ * 	     specified by the user. It also measures the amount of time taken
+ *	     to carry out the invert operation.
+ *   @Tools_Used GCC
+​ * ​ ​ @author​ ​Devansh Mittal, Souvik De
+​ * ​ ​ @date​ ​ October 3rd, 2018
+​ * ​ ​ @version​ ​ 1.0
+*****************************************************************************/
+
 #include<stdio.h>
 #include<time.h>
 #include"allocate.h"
@@ -20,39 +31,43 @@ void invert_block(void)
 		do
 		{
 			printf("\nProvide address index to invert memory bits (starts from '0') : ");
-			scanf("%u",&address);
+			scanf("%u",&address); /* Accepts the offset value from the starting memory address to invert data bits */
 
-			if(!IsWithinBounds(address))
+			if(!IsWithinBounds(address)) /* Calls function and checks if the address offset inputted by user is within bounds */
 			{
 				printf("\nUnacceptable input, index is out of bounds!\n");
 				printf("1-to enter again and 0-ignore : ");
 				scanf("%hhu",&choice);
 			}
 		}
-		while(!IsWithinBounds(address) && choice);
+		while(!IsWithinBounds(address) && choice); /* Keep looping if address is outside bounds and user wishes to input again */
 
 		do
 		{		
 			printf("\nHow many words would you like to invert? ");
-			scanf("%u",&num_blocks);
+			scanf("%u",&num_blocks); /* Accepts the number of 32 bit words from which to invert data bits */
 
-			if(!IsWithinBounds(address + num_blocks - 1))
+			if(!IsWithinBounds(address + num_blocks - 1)) /* Calls function and checks if number of words goes outside the allocated address range */
 			{
 				printf("\nUnacceptable input, index is out of bounds!\n");
 				printf("1-to enter again and 0-ignore : ");
 				scanf("%hhu",&choice);
 			}
 		}
-		while(!IsWithinBounds(address + num_blocks - 1) && choice);
+		while(!IsWithinBounds(address + num_blocks - 1) && choice); /* Keep looping if number of words crosses bounds and user wishes to input again */
 		
-		BeginCapture = clock();
+		BeginCapture = clock(); /* Start clock timer */
 		for(parser = address ; parser < address + num_blocks ; parser++)
 		{
-			*(memory + parser) ^= 4294967295;
+			*(memory + parser) ^= 4294967295; /* XOR operation with 'FFFFFFFF' to invert data bits */
 		}
-		EndCapture = clock();
+		EndCapture = clock(); /* End clock timer */
 		
-		printf("\nMemory blocks were successfully inverted in %f secs\n",(double)(EndCapture-BeginCapture)/CLOCKS_PER_SEC);
+		printf("\nMemory blocks were successfully inverted in %f secs\n",(double)(EndCapture-BeginCapture)/CLOCKS_PER_SEC); 
+		/**
+		* In the above calculation, the difference provides us with the number of clock ticks elapsed. To get time taken in
+		* seconds, we divide it by the number of clock ticks per second which depends on the system.
+		*/
 		
 	}	
 }
--- invblk.h
+++ invblk.h
@@ -1,1 +1,21 @@
+/*****************************************************************************
+​ ​* ​ ​ @file​ ​  invblk.h
+​ * ​ ​ @brief​ ​ This header file contains the function declaration of the invert
+ *	     block function.
+ *   @Tools_Used GCC
+​ * ​ ​ @author​ ​Devansh Mittal, Souvik De
+​ * ​ ​ @date​ ​ October 3rd, 2018
+​ * ​ ​ @version​ ​ 1.0
+*****************************************************************************/
+
+/**
+​ * ​ ​ @brief​ ​ Inverts the data bits present at the addresses specified by the
+ *	     user. It also measures the amount of time taken to carry out the
+ *	     invert operation.
+ *
+​ * ​ ​ @param​ ​ void
+​ *
+​ * ​ ​ @return void
+ */
+
 void invert_block(void);
