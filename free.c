e7c0dd09bdbcb2be4fe3e3c46ca08c66eb89323a
Devansh Mittal <demi1671@colorado.edu>
2018-10-03 20:58:20 -0600

    Added comments and file details

--- free.c
+++ free.c
@@ -1,15 +1,25 @@
+/*****************************************************************************
+​ ​* ​ ​ @file​ ​  free.h
+​ * ​ ​ @brief​ ​ This file frees up whatever memory was allocated by the malloc
+ *	     function in the allocate function. 
+ *   @Tools_Used GCC
+​ * ​ ​ @author​ ​Devansh Mittal, Souvik De
+​ * ​ ​ @date​ ​ October 3rd, 2018
+​ * ​ ​ @version​ ​ 1.0
+*****************************************************************************/
+
 #include<stdio.h>
 #include"allocate.h"
 
 void free_mem(void)
 {
-	if(memory == NULL)
+	if(memory == NULL) /* Checks if memory is allocated */
 	{
 		printf("\nMemory not allocated, allocate and try again...!\n");
 	}
 	else
 	{
-		free(memory);
+		free(memory); /* Command to free allocated memory */
 		memory = NULL;
 		printf("\n%lu bytes of memory has been freed\n",size * sizeof(uint32_t));
 	}
--- free.h
+++ free.h
@@ -1,1 +1,21 @@
+/*****************************************************************************
+​ ​* ​ ​ @file​ ​  display.h
+​ * ​ ​ @brief​ ​ This header file contains the function declaration of the free
+ *	     function which deletes memory allocated by the malloc command
+ *	     in the allocate function.
+ *   @Tools_Used GCC
+​ * ​ ​ @author​ ​Devansh Mittal, Souvik De
+​ * ​ ​ @date​ ​ October 3rd, 2018
+​ * ​ ​ @version​ ​ 1.0
+*****************************************************************************/
+
+/**
+​ * ​ ​ @brief​ ​ deletes memory allocated by the malloc command in the allocate 
+ *	     function.
+ *
+​ * ​ ​ @param​ ​ void
+​ *
+​ * ​ ​ @return void
+ */
+
 void free_mem(void);
