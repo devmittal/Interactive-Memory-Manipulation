b7621d509d752ed26034cf8a3dad1cf962f826c0
Devansh Mittal <demi1671@colorado.edu>
2018-10-04 00:14:18 -0600

    Deleted the redundant controller.h file

--- controller.h
+++
@@ -1,9 +0,0 @@
-void help(void);
-void allocate_mem(void);
-void free_mem(void);
-void display_mem(void);
-void write_mem(void);
-void invert_block(void);
-void write_pattern(void);
-void verify_pattern(void);
-
