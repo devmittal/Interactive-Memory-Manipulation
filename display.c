3ad9374448a01f1c281ae1f1a607aa36ebb67371
Devansh Mittal <demi1671@colorado.edu>
2018-10-03 20:45:16 -0600

    Added comments and file details

--- display.c
+++ display.c
@@ -1,3 +1,13 @@
+/*****************************************************************************
+​ ​* ​ ​ @file​ ​  display.c
+​ * ​ ​ @brief​ ​ This file displays a table with the range of addresses chosen by 
+ *	     the user and the data present in those addresses. 
+ *   @Tools_Used GCC
+​ * ​ ​ @author​ ​Devansh Mittal, Souvik De
+​ * ​ ​ @date​ ​ October 3rd, 2018
+​ * ​ ​ @version​ ​ 1.0
+*****************************************************************************/
+
 #include<stdio.h>
 #include"allocate.h"
 
@@ -18,33 +28,33 @@ void display_mem(void)
 		do
 		{
 			printf("\nEnter address index to see contiguous data : ");
-			scanf("%u",&address);
+			scanf("%u",&address); /* Accepts the offset value from the starting memory address to display data present in those locations */
 			
-			if(!IsWithinBounds(address))
+			if(!IsWithinBounds(address)) /* Calls function and checks if the address inputted by user is within bounds */
 			{
-				printf("\nUnacceptable input, index is out of bounds!\n");
+				printf("\nUnacceptable input, index is out of bounds!\n"); 
 				printf("1-to enter again and 0-ignore : ");
 				scanf("%hhu",&choice);
 			}
 		}
-		while(!IsWithinBounds(address) && choice);
+		while(!IsWithinBounds(address) && choice); /* Keep looping if address is outside bounds and user wishes to input again */
 		
 		do
 		{		
 			printf("\nHow many words would you like to see? ");
-			scanf("%u",&num_blocks);
+			scanf("%u",&num_blocks); /* Accepts the number of 32 bit words from which to display data */
 
-			if(!IsWithinBounds(address + num_blocks - 1))
+			if(!IsWithinBounds(address + num_blocks - 1)) /* Calls function and checks if number of words goes outside the allocated address range */
 			{
 				printf("\nUnacceptable input, index is out of bounds!\n");
 				printf("1-to enter again and 0-ignore : ");
 				scanf("%hhu",&choice);
 			}
 		}
-		while(!IsWithinBounds(address + num_blocks - 1) && choice);
+		while(!IsWithinBounds(address + num_blocks - 1) && choice); /* Keep looping if number of words crosses bounds and user wishes to input again */
 		
 
-		printf("\nIndex\tAddress \tData");
+		printf("\nIndex\tAddress \tData"); /* Displays table of Index, Address and data present at the address */
 		printf("\n_____\t_______ \t____\n");
 		
 		for(parser = address ; parser < address + num_blocks ; parser++)
--- display.h
+++ display.h
@@ -1,1 +1,20 @@
+/*****************************************************************************
+​ ​* ​ ​ @file​ ​  display.h
+​ * ​ ​ @brief​ ​ This header file contains the function declaration of the display
+ *	     function which displays the data present in the addresses selected
+ *	     by the user. 
+ *   @Tools_Used GCC
+​ * ​ ​ @author​ ​Devansh Mittal, Souvik De
+​ * ​ ​ @date​ ​ October 3rd, 2018
+​ * ​ ​ @version​ ​ 1.0
+*****************************************************************************/
+
+/**
+​ * ​ ​ @brief​ ​ Displays the data present in the addresses selected by the user.
+ *
+​ * ​ ​ @param​ ​ void
+​ *
+​ * ​ ​ @return void
+ */
+
 void display_mem(void);
