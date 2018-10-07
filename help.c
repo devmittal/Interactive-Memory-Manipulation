ce178037327eb30947cac216b3c37fb64f535f5f
Souvik De <souvik.de@colorado.edu>
2018-10-07 17:46:53 -0600

    Changed command line form number driven to string driven format. Changes in main and help

--- help.c
+++ help.c
@@ -16,7 +16,7 @@ void help()
 	printf("\nHelp Menu");
 	printf("\n*********\n");
 	printf("\nSelect from the following in the main menu:\n");
-	printf("\nAllocate Memory (2)\nFree Memory (3)\nDisplay Memory (4)\n");
-	printf("Write Memory (5)\nInvert Block (6)\nWrite Pattern (7)\n");
-	printf("Verify Pattern (8)\nExit (9)\n");
+	printf("\nAllocate Memory - 'alloc'\nFree Memory - 'free'\nDisplay Memory - 'disp'\n");
+	printf("Write Memory - 'write'\nInvert Block - 'inv'\nWrite Pattern - 'wrpat'\n");
+	printf("Verify Pattern - 'verpat'\nExit - 'exit'\n");
 }
--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 ​ *	     greeting and the function calls for each feature according to
 ​ * ​ ​ 	     user input.
  *   @Tools_Used GCC
-​ * ​ ​ @author​ ​Devansh Mittal, Souvik De
+​ * ​ ​ @author​ ​Souvik De, Devansh Mittal
 ​ * ​ ​ @date​ ​ October 3rd, 2018
 ​ * ​ ​ @version​ ​ 1.0
 *****************************************************************************/
 
 #include<stdio.h>
+#include<string.h>
 #include"help.h"
 #include"allocate.h"
 #include"display.h"
@@ -19,56 +20,43 @@
 #include"write.h"
 #include"wrpatt.h"
 
+void quit(void);
+
 int main()
 {
-	int response;
+	char response[20] = {};
+	char* commands[10] = {"help","alloc","free","disp","write","inv","wrpat","verpat","exit"};
+	void (*func[])(void) = {help,allocate_mem,free_mem,display_mem,write_mem,invert_block,write_pattern,verify_pattern,quit};	
+	unsigned char i = 0;
 	
 	printf("\n\nWelcome to the Interactive Memory Manipulation");
 	printf("\n**********************************************\n");
 
 	do
-	{		
-		printf("\nEnter 1 for help");
-		printf("\nResponse : ");
-		scanf("%d",&response); /* User selects which feature is to be implemented here */
-
-		switch(response)
+	{
+		printf("\nWrite 'help' for Menu\n>>");
+		scanf("%s",response);
+		
+		for(i = 0 ; i < 9 ; i++)
 		{
-			case 1:	help();
+			if(strcmp(response,commands[i]) == 0)
+			{
+				func[i]();
 				break;
-
-			case 2:	allocate_mem();
-				break;
-
-			case 3:	free_mem();
-				break;
-
-			case 4:	display_mem();
-				break;
-
-			case 5:	write_mem();
-				break;
-
-			case 6:	invert_block();
-				break;
-
-			case 7:	write_pattern();
-				break;
-
-			case 8:	verify_pattern();
-				break;
-
-			case 9:	printf("\nEnd of Program\n\n");
-				break;
-
-			default:printf("\nInvalid Response, Try again\n");
-				break;
+			}
 		}
 
+		if(i == 9)
+		{
+			printf("\nInvalid Command..!\n");
+		}
 	}
-	while(response != 9);
-
+	while(strcmp(response,"exit") != 0);
 	
-
 	return 0;
+}
+
+void quit(void)
+{
+	exit(0);
 }
